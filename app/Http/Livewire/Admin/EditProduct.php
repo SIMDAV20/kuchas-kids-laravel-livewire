dc37865b2d35e925d18f396cea7edd527b2fd42a
DAVID SIMON BOLIVAR CASTRO <david14847@gmail.com>
2024-03-19 00:39:38 -0500

    change validation

--- app/Http/Livewire/Admin/EditProduct.php
+++ app/Http/Livewire/Admin/EditProduct.php
@@ -40,7 +40,7 @@ class EditProduct extends Component
         'product.description'    => 'required',
         'product.quantity'       => '',
         'product.brand_id'       => '',
-        'product.price'          => 'required|numeric|min:2',
+        'product.price'          => '',
         'product.offer_price'    => 'nullable|lt:product.price',
         'product.video'          => 'nullable',
     ];
@@ -286,8 +286,8 @@ class EditProduct extends Component
 
         if (count($this->product->color_product) == 0 && count($this->product->product_size) == 0 && count($this->product->color_product_size) == 0) {
             $rules['product.quantity'] = 'required|numeric|min:1';
+            $rules['product.price'] = 'required|numeric|min:2';
         }
-
         $this->validate($rules);
 
         $this->product->slug = $this->slug;
